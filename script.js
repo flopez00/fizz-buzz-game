dd70d6491d65f379a0d8c84958dce3b3b43094bd
Francisco <franciscolopezjr012@gmail.com>
2021-09-19 20:34:36 -0500

    JS code for checking the numbers

---
+++ script.js
@@ -0,0 +1,13 @@
+const fizzBuzzBtn = document.getElementsByClassName('.fizzBuzzBtn');
+
+let answer = parseInt(prompt('Please enter a number to Fizz Buzz to!: '));
+
+for (let i = 1; i <= answer; i++) {
+   if (i % 3 === 0 && i % 5 ===0) {
+       console.log('FizzBuzz')
+   } else if (i % 3 === 0) {
+       console.log('Fizz')
+   } else if (i % 5 === 0) {
+       console.log('Buzz')
+   }else console.log(i)
+}
